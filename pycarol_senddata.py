c7af40bd79d2a8f91c7ddac859bd256f01cdb890
brenopapa <bpapa@outlook.com.br>
2022-12-19 15:53:57 -0300

    Fixing connector name.

--- pycarol_senddata.py
+++ pycarol_senddata.py
@@ -3,48 +3,54 @@ import pandas as pd
 from pycarol import PwdAuth, Carol, Staging, ApiKeyAuth, Connectors
 from pycarol.bigquery import BQ
 
-credential_file = "/Users/brenozipoli/Desktop/login.json"
-with open(credential_file, encoding="utf-8") as f:
-    auth = json.loads(f.read())
+mytenants = {
+    'brenopapa1': '',
+    'brenopapa2': '',
+    'brenopapa3': ''
+}
 
-carol = Carol(domain='brenopapa', #app_name='MinhaCoop',
-              auth=PwdAuth(auth['username'], auth['password']), organization='datascience')
+for tenant, connector in mytenants.items():
+    print(tenant, connector)
+    credential_file = "/Users/brenozipoli/Desktop/carol.json"
+    with open(credential_file, encoding="utf-8") as f:
+        auth = json.loads(f.read())
 
-api_key = carol.issue_api_key()
+    carol = Carol(domain=tenant, #app_name='MinhaCoop',
+                auth=PwdAuth(auth['username'], auth['password']), organization='datascience') 
 
-connector = 'f85319e5c6f04042b10c127d0e66dbcf'
+    api_key = carol.issue_api_key()
 
-df = pd.read_csv("./data/airports.dat", names=["Airport_ID", "Name", "City", "Country", "IATA", "ICAO", "Latitude", "Longitude", "Altitude", "Timezone", "DST", "Tz_database_time_zone", "Type", "Source"])
-print(df.head())
+    df = pd.read_csv("./data/airports.dat", names=["Airport_ID", "Name", "City", "Country", "IATA", "ICAO", "Latitude", "Longitude", "Altitude", "Timezone", "DST", "Tz_database_time_zone", "Type", "Source"])
+    # print(df.head())
 
-staging = Staging(carol)
-staging.send_data(staging_name = 'airports', data = df.astype(str), step_size = 1000,
-                 connector_id=connector, print_stats = True, force=True)
+    staging = Staging(carol)
+    staging.send_data(staging_name = 'airports', data = df.astype(str), step_size = 1000,
+                    connector_id=connector, print_stats = True, force=True)
 
-df = pd.read_csv("./data/airlines.dat", names=["Airline_ID", "Name", "Alias", "IATA", "ICAO", "Callsign", "Country", "Active"])
-print(df.head())
+    df = pd.read_csv("./data/airlines.dat", names=["Airline_ID", "Name", "Alias", "IATA", "ICAO", "Callsign", "Country", "Active"])
+    # print(df.head())
 
-staging = Staging(carol)
-staging.send_data(staging_name = 'airlines', data = df.astype(str), step_size = 1000,
-                 connector_id=connector, print_stats = True, force=True)
+    staging = Staging(carol)
+    staging.send_data(staging_name = 'airlines', data = df.astype(str), step_size = 1000,
+                    connector_id=connector, print_stats = True, force=True)
 
-df = pd.read_csv("./data/countries.dat", names=["name", "iso_code", "dafif_code"])
-print(df.head())
+    df = pd.read_csv("./data/countries.dat", names=["name", "iso_code", "dafif_code"])
+    # print(df.head())
 
-staging = Staging(carol)
-staging.send_data(staging_name = 'countries', data = df.astype(str), step_size = 1000,
-                 connector_id=connector, print_stats = True, force=True)
+    staging = Staging(carol)
+    staging.send_data(staging_name = 'countries', data = df.astype(str), step_size = 1000,
+                    connector_id=connector, print_stats = True, force=True)
 
-df = pd.read_csv("./data/planes.dat", names=["Name", "IATA_code", "ICAO_code"])
-print(df.head())
+    df = pd.read_csv("./data/planes.dat", names=["Name", "IATA_code", "ICAO_code"])
+    # print(df.head())
 
-staging = Staging(carol)
-staging.send_data(staging_name = 'planes', data = df.astype(str), step_size = 1000,
-                 connector_id=connector, print_stats = True, force=True)
+    staging = Staging(carol)
+    staging.send_data(staging_name = 'planes', data = df.astype(str), step_size = 1000,
+                    connector_id=connector, print_stats = True, force=True)
 
-df = pd.read_csv("./data/routes.dat", names=["Airline", "Airline_ID", "Source_airport", "Source_airport_ID", "Destination_airport", "Destination_airport_ID", "Codeshare", "Stops", "Equipment"])
-print(df.head())
+    df = pd.read_csv("./data/routes.dat", names=["Airline", "Airline_ID", "Source_airport", "Source_airport_ID", "Destination_airport", "Destination_airport_ID", "Codeshare", "Stops", "Equipment"])
+    # print(df.head())
 
-staging = Staging(carol)
-staging.send_data(staging_name = 'routes', data = df.astype(str), step_size = 1000,
-                 connector_id=connector, print_stats = True, force=True)
+    staging = Staging(carol)
+    staging.send_data(staging_name = 'routes', data = df.astype(str), step_size = 1000,
+                    connector_id=connector, print_stats = True, force=True)
