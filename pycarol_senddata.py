d7a2df680ea0677fe9e61be851207e43f7fb4cc7
brenopapa <bpapa@outlook.com.br>
2022-12-20 13:57:55 -0300

    Adjusting pipeline staging tables.

--- pycarol_senddata.py
+++ pycarol_senddata.py
@@ -4,7 +4,8 @@ from pycarol import PwdAuth, Carol, Staging, ApiKeyAuth, Connectors
 from pycarol.bigquery import BQ
 
 mytenants = {
-    'brenopapa2': '87f97dacdab54da98b673eee16f93ca0'
+    'brenopapa2': 'a007680ea1874bb5a337ca399bd4cf60',
+    'brenopapa3': 'ea9db33535f4455a901568effc9607f5',
 }
 
 for tenant, connector in mytenants.items():
